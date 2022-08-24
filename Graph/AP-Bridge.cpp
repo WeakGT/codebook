aaa4e1862767b942be880b1741d482867340c613
WeakGT <111994847+WeakGT@users.noreply.github.com>
2022-08-25 00:14:10 +0800

    Create AP-Bridge.cpp

---
+++ Graph/AP-Bridge.cpp
@@ -0,0 +1,26 @@
+#define eb emplace_back
+using pii = pair<int, int>;
+constexpr int N = 2e5 + 1;
+
+int d[N], low[N];
+vector<int> g[N];
+vector<int> ap;
+vector<pii> bridge;
+
+void dfs(int x, int p) {
+	d[x] = ~p ? d[p] + 1 : 1, low[x] = d[x];
+	int cnt = 0;
+	bool isap = 0;
+	for (const auto& i : g[x]) {
+		if (i == p) continue;
+		if (!d[i]) {
+			dfs(i, x), cnt++;
+			if (d[x] <= low[i]) isap = 1;
+			if (d[x] < low[i]) bridge.eb(x, i);
+			low[x] = min(low[x], low[i]);
+		}
+		low[x] = min(low[x], d[i]);
+	}
+	if (p == -1 && cnt < 2) isap = 0;
+	if (isap) ap.eb(x);
+}
