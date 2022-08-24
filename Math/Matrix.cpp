9453f028d4ee9bb01422d154542e95b7618feaa3
WeakGT <111994847+WeakGT@users.noreply.github.com>
2022-08-24 22:43:56 +0800

    Create Matrix.cpp

---
+++ Math/Matrix.cpp
@@ -0,0 +1,14 @@
+template <typename T> using vec = vector<T>;
+template <typename T> using matrix = vec<vec<T>>;
+constexpr int mod = 1e9 + 7;
+
+template <typename T>
+matrix<T> operator*(const matrix<T>& a, const matrix<T>& b) {
+	int n = a.size(), r = b.size(), m = b.front().size();
+	matrix<T> ret(n, vec<T>(m));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			for (int k = 0; k < r; k++)
+				ret[i][j] += 1LL * a[i][k] * b[k][j] % mod, ret[i][j] %= mod;
+	return ret;
+}
