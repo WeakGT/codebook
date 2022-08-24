bbf95e1de62950152639c6de7d3e4b8c42148bf7
WeakGT <111994847+WeakGT@users.noreply.github.com>
2022-08-25 00:36:25 +0800

    Create BCC.cpp

---
+++ Graph/BCC.cpp
@@ -0,0 +1,24 @@
+#define ep emplace
+constexpr int N = 2e5 + 1;
+
+int d[N], low[N], bcc[N], id;
+vector<int> g[N];
+stack<int> st;
+
+void dfs(int x, int p) {
+	d[x] = ~p ? d[p] + 1 : 1, low[x] = d[x];
+	st.ep(x);
+	for (const auto& i : g[x]) {
+		if (i == p) continue;
+		if (!d[i]) {
+			dfs(i, x);
+			low[x] = min(low[x], low[i]);
+		}
+		low[x] = min(low[x], d[i]);
+	}
+	if (low[x] == d[x]) {
+		id++;
+		int xx;
+		do xx = st.top(), st.pop(), bcc[xx] = id; while (xx != x);
+	}
+}
