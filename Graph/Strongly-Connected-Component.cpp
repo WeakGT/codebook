a40db0ac86db123ac27b68cf40bf7ada289192c8
WeakGT <111994847+WeakGT@users.noreply.github.com>
2022-08-25 23:32:37 +0800

    Create Strongly-Connected-Component.cpp

---
+++ Graph/Strongly-Connected-Component.cpp
@@ -0,0 +1,20 @@
+#define ep emplace
+constexpr int N = 2e5 + 1;
+
+int d[N], low[N], scc[N], ins[N], id;
+vector<int> g[N];
+stack<int> st;
+
+void dfs(int x, int p) {
+	d[x] = ~p ? d[p] + 1 : 1, low[x] = d[x];
+	st.ep(x), ins[x] = 1;
+	for (const auto& i : g[x]) {
+		if (!d[i]) dfs(i, x), low[x] = min(low[x], low[i]);
+		else if (ins[i]) low[x] = min(low[x], d[i]);
+	}
+	if (d[x] == low[x]) {
+		int tmp;
+		do tmp = st.top(), st.pop(), scc[tmp] = id, ins[tmp] = 0; while (tmp != x);
+		id++;
+	}
+}
